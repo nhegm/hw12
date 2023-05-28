af948c1b0ded86cb5fec4a4745f9ac5563bb97b6
nhegmus@gmail.com <nhegm>
2023-05-28 20:44:17 +0800

    4 classes added (getTitle, findAll, findLast, findLast5) and few tests for them

---
+++ src/main/java/Cinema.java
@@ -0,0 +1,36 @@
+public class Cinema {
+    private String[] titles = new String[0];
+    private int amountOfTitles = 5;
+
+    public void addTitle(String title) {
+        String[] tmp = new String[titles.length + 1];
+        for (int i = 0; i < titles.length; i++) {
+            tmp[i] = titles[i];
+        }
+        tmp[tmp.length - 1] = title;
+        titles = tmp;
+    }
+
+    public String[] findAll() {
+        return titles;
+    }
+
+    public String[] findLast() {
+
+        String[] reversed5 = new String[5];
+        for (int i = 0; i < 5; i++) {
+            reversed5[i] = titles[titles.length - 1 - i];
+        }
+        return reversed5;
+    }
+
+    public String[] findLast(int amountOfTitles) {
+        String[] reversed = new String[amountOfTitles];
+        this.amountOfTitles = amountOfTitles;
+
+        for (int i = 0; i < amountOfTitles; i++) {
+            reversed[i] = titles[titles.length - 1 - i];
+        }
+        return reversed;
+    }
+}
---
+++ src/test/java/CinemaTest.java
@@ -0,0 +1,79 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class CinemaTest {
+    String title1 = "Номер один";
+    String title2 = "Bloodshot";
+    String title3 = "Джентельмены";
+    String title4 = "Тролли. Тролли наносят ответный удар";
+    String title5 = "Человек-невидимка";
+    String title6 = "Вперёд";
+    String title7 = "Отель «Белогорск»";
+
+    @Test
+    public void ShouldAddTitleThenShow() {
+        Cinema cinemaTesting = new Cinema();
+        cinemaTesting.addTitle(title1);
+        cinemaTesting.addTitle(title2);
+        cinemaTesting.addTitle(title3);
+        cinemaTesting.findAll();
+        String[] expected = {title1, title2, title3};
+        String[] actual = cinemaTesting.findAll();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldAddTitleThenShowReversedArrayLength5() {
+
+        Cinema cinemaTesting = new Cinema();
+        cinemaTesting.addTitle(title1);
+        cinemaTesting.addTitle(title2);
+        cinemaTesting.addTitle(title3);
+        cinemaTesting.addTitle(title4);
+        cinemaTesting.addTitle(title5);
+        cinemaTesting.addTitle(title6);
+        cinemaTesting.addTitle(title7);
+        cinemaTesting.findLast();
+        String[] expected = {title7, title6, title5, title4, title3};
+        String[] actual = cinemaTesting.findLast();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldAddTitleThenShowReversedArrayLength3() {
+
+        Cinema cinemaTesting = new Cinema();
+        cinemaTesting.addTitle(title1);
+        cinemaTesting.addTitle(title2);
+        cinemaTesting.addTitle(title3);
+        cinemaTesting.addTitle(title4);
+        cinemaTesting.addTitle(title5);
+        cinemaTesting.addTitle(title6);
+        cinemaTesting.addTitle(title7);
+        cinemaTesting.findLast(3);
+        String[] expected = {title7, title6, title5};
+        String[] actual = cinemaTesting.findLast(3);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShouldAddTitleThenShowReversedArrayLength6() {
+
+        Cinema cinemaTesting = new Cinema();
+        cinemaTesting.addTitle(title1);
+        cinemaTesting.addTitle(title2);
+        cinemaTesting.addTitle(title3);
+        cinemaTesting.addTitle(title4);
+        cinemaTesting.addTitle(title5);
+        cinemaTesting.addTitle(title6);
+        cinemaTesting.addTitle(title7);
+        cinemaTesting.findLast(6);
+        String[] expected = {title7, title6, title5, title4, title3, title2};
+        String[] actual = cinemaTesting.findLast(6);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
